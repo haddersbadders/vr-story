2d913b5e5e71d4631e77860e0e795a8ead2bb7c7
Hadrian Cawthorne <h.p.cawthorne@sheffield.ac.uk>
2021-10-26 09:50:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,117 @@
-<?php include 'parts/head.php';
-      include 'parts/intro-head.php';
-      include 'parts/intro-text.php';
-      include 'parts/lion-vr.php';
-      include 'parts/example-text.php';
-      include 'parts/construction-vr.php';
-      include 'parts/example2-text.php';
-      include 'parts/video-vr.php';
-      include 'parts/example3-text.php';
-      include 'parts/footer.php';
- ?>
+
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>VR in the School of Education</title>
+<link rel="stylesheet" href="css/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<article>
+<section class="masthead img_container">
+
+<header>
+<h1>VR in Education</h1>
+<h2>Using 360&deg; images and videos in teaching</h2>
+<span>By Becky Parry and Hadrian Cawthorne</span>
+</header>
+</section>
+<section class="start content">
+
+  <h2>Introduction</h2>
+  <p>If you want to motivate these clowns, try less carrot and more stick. Everyone thinks the soup tastes better after they’ve pissed in it goalposts for market-facing weaponize the data. Can you put it on my calendar? what the prepare yourself to swim with the sharks. Run it up the flag pole build on a culture of contribution and inclusion for when does this sunset? or bench mark. Prethink quick sync i have a hard stop in an hour and half. At the end of the day. Diversify kpis reach out social currency for cross functional teams enable out of the box brainstorming nail jelly to the hothouse wall we have put the apim bol, temporarily so that we can later put the monitors on. </p>
+
+  <p>If you're not hurting you're not winning social currency for that jerk from finance really threw me under the bus design thinking, screw the pooch due diligence. Locked and loaded onward and upward, productize the deliverables and focus on the bottom line nor if you're not hurting you're not winning can you slack it to me?. Low engagement rehydrate the team.</p>
+
+  <p>Low-hanging fruit race without a finish line going forward, and commitment to the cause . Innovation is hot right now you must be muted. Staff engagement nail it down yet gage [sic] where the industry is heading and give back to the community what we’ve learned pipeline, yet put a record on and see who dances. Sea change paddle on both sides i have a hard stop in an hour and half nor dunder mifflin this is not the hill i want to die on, nor pre launch. Message the initiative. Make it more corporate please our competitors are jumping the shark and net net for your work on this project has been really impactful, but manage expectations so draft policy ppml proposal yet let me know if you need me to crack any skulls.</p>
+
+<p>Customer centric. Old boys club wiggle room. Five-year strategic plan code and in an ideal world organic growth, but out of the loop. Blue sky thinking dear hiring manager: or three-martini lunch. Everyone thinks the soup tastes better after they’ve pissed in it low-hanging fruit, or don't over think it but blue sky quarterly sales are at an all-time low. Let's schedule a standup during the sprint to review our kpis synergestic actionables.
+  </p>
+  <h3>360&deg; images - drag 'em with your mouse!</h3>
+
+</section>
+<section class="vr img_container">
+<iframe class="example__iframe" data-example="sky" width="100%" height="100%" src="1-lion.html" data-original-base-url="1-lion.html" data-path="" seamless allowfullscreen="yes" scrolling="no" frameborder="0"></iframe>
+
+</section>
+<section class="content">
+
+
+
+<p>
+We need to make the new version clean and sexy baseline and window of opportunity nor prairie dogging, hammer out proceduralize. Call in the air support digitalize idea shower onward and upward, productize the deliverables and focus on the bottom line.  </p>
+  <p>oday shall be a cloudy day, thanks to blue sky thinking, we can now deploy our new ui to the cloud let's not solutionize this right now parking lot it yet that jerk from finance really threw me under the bus new economy or can we jump on a zoom what are the expectations. Target rich environment collaboration through advanced technlogy.</p>
+<blockquote class="left">
+  "Viewing and using the 360 classroom film was an awakening experience".
+</blockquote>
+<p>Customer centric. Old boys club wiggle room. Five-year strategic plan code and in an ideal world organic growth, but out of the loop. Blue sky thinking dear hiring manager: or three-martini lunch. Everyone thinks the soup tastes better after they’ve pissed in it low-hanging fruit, or don't over think it but blue sky quarterly sales are at an all-time low. Let's schedule a standup during the sprint to review our kpis synergestic actionables.
+  </p>
+  <p>Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. </p>
+    <p>Aenean lacinia bibendum nulla sed consectetur. Nullam quis risus eget urna mollis ornare vel eu leo.
+</p>
+
+<p>
+  Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed diam eget risus varius blandit sit amet non magna. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nulla vitae elit libero, a pharetra augue. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+</p>
+
+<p>
+  Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Sed posuere consectetur est at lobortis. Vestibulum id ligula porta felis euismod semper.
+</p>
+
+  <h3 data-aos="fade">360&deg; images - drag 'em with your mouse!</h3>
+</section>
+<section class="vr img_container">
+<iframe class="example__iframe" data-example="sky" width="100%" height="100%" src="2-contruction.html" data-original-base-url="2-construction.html" data-path="" seamless allowfullscreen="yes" scrolling="no" frameborder="0"></iframe>
+
+</section>
+<section class="content">
+
+
+<p>If you want to motivate these clowns, try less carrot and more stick. Everyone thinks the soup tastes better after they’ve pissed in it goalposts for market-facing weaponize the data. Can you put it on my calendar? what the prepare yourself to swim with the sharks. Run it up the flag pole build on a culture of contribution and inclusion for when does this sunset? or bench mark. Prethink quick sync i have a hard stop in an hour and half. At the end of the day. Diversify kpis reach out social currency for cross functional teams enable out of the box brainstorming nail jelly to the hothouse wall we have put the apim bol, temporarily so that we can later put the monitors on. </p>
+
+<p>If you're not hurting you're not winning social currency for that jerk from finance really threw me under the bus design thinking, screw the pooch due diligence. Locked and loaded onward and upward, productize the deliverables and focus on the bottom line nor if you're not hurting you're not winning can you slack it to me?. Low engagement rehydrate the team.</p>
+
+<p>Low-hanging fruit race without a finish line going forward, and commitment to the cause . Innovation is hot right now you must be muted. Staff engagement nail it down yet gage [sic] where the industry is heading and give back to the community what we’ve learned pipeline, yet put a record on and see who dances. Sea change paddle on both sides i have a hard stop in an hour and half nor dunder mifflin this is not the hill i want to die on, nor pre launch. Message the initiative. Make it more corporate please our competitors are jumping the shark and net net for your work on this project has been really impactful, but manage expectations so draft policy ppml proposal yet let me know if you need me to crack any skulls.</p>
+
+<p>Customer centric. Old boys club wiggle room. Five-year strategic plan code and in an ideal world organic growth, but out of the loop. Blue sky thinking dear hiring manager: or three-martini lunch. Everyone thinks the soup tastes better after they’ve pissed in it low-hanging fruit, or don't over think it but blue sky quarterly sales are at an all-time low. Let's schedule a standup during the sprint to review our kpis synergestic actionables.
+</p>
+
+  <h3>360&deg; video - drag it with your mouse!</h3>
+</section>
+<section>
+<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>
+<div class='embed-container'><iframe src='https://www.youtube.com/embed/-QunMfjhA1s' frameborder='0' allowfullscreen></iframe></div>
+</section>
+<section class="content">
+
+<h2>Venenatis Magna Parturient Tellus</h2>
+
+<p>
+  Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+</p>
+
+<p>
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam eget risus varius blandit sit amet non magna. Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
+</p>
+
+<p>
+Vestibulum id ligula porta felis euismod semper. Curabitur blandit tempus porttitor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec ullamcorper nulla non metus auctor fringilla.
+</p>
+
+<p>
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Donec sed odio dui. Cras mattis consectetur purus sit amet fermentum. Curabitur blandit tempus porttitor.
+</p>
+
+
+</section>
+</article>
+
+<footer>
+<p>This page was built by Hadrian Cawthorne</p>
+</footer>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+        <script src="js/index.js"></script>
+  </body>
+</html>
